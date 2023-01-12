32d726341af91db080757647da14550b90fbe6e5
komalk <komalk@futurismtechnologies.com>
2023-01-12 22:53:52 +0530

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,18 +40,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.sendgrid.net",
-  #   domain: "gmail.com",
-  #   port: '465',
-  #   user_name: 'Mailapi',
-  #   password: ENV["password"],
-  #   authentication: :plain,
-  #   enable_starttls_auto: true,
-  #   openssl_verify_mode:  'none',
-  #   ssl: true,
-  #   tls: true
-  # }
+  
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.

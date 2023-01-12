0a85f1db37288d499c8706477b40e8eef8f7e697
komalk <komalk@futurismtechnologies.com>
2023-01-12 22:53:24 +0530

    integrate sendgrid api for email notification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,15 +40,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
-    port: 587,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    authentication: "plain",
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.sendgrid.net",
+  #   domain: "gmail.com",
+  #   port: '465',
+  #   user_name: 'Mailapi',
+  #   password: ENV["password"],
+  #   authentication: :plain,
+  #   enable_starttls_auto: true,
+  #   openssl_verify_mode:  'none',
+  #   ssl: true,
+  #   tls: true
+  # }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
